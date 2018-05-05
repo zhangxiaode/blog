2515513a5cb7e5046cd14072fc97be4f6d737d76
zxd <1243019627@qq.com>
2018-05-05 18:30:30 +0800

    调试

--- src/view/App.js
+++ src/view/App.js
@@ -1,15 +1,10 @@
 import React, { Component } from 'react';
-import logo from '../assets/logo.svg';
-import '../style/app.less';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+        blog
       </div>
     );
   }
