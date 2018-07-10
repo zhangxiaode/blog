b43eb4425bcea85a6f85c9e98b21c92b1be19ec1
wang <1281602841@qq.com>
2018-07-10 10:48:52 +0800

    首页修改

--- src/view/index/index.jsx
+++ src/view/index/index.jsx
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
-
+import { Link } from 'react-router-dom';
+import './index.less';
 class App extends Component {
   render() {
     return (
       <div className="homepage">
-        this is homepage.
+        <div className="sideBar">
+          <div className="categoryList">
+            <Link to="/">消息提醒</Link>
+            <Link to="/">写文章</Link>
+            <Link to="/">留言板</Link>
+            <Link to="/">私信</Link>
+          </div>
+          <div className="card">
+            <Link to="/">收藏文章</Link>
+            <Link to="/">收藏社区</Link>
+            <Link to="/">我的关注</Link>
+            <Link to="/">关注我的</Link>
+          </div>
+        </div>
+        <div className="content">
+          this is content
+        </div>
       </div>
     );
   }
